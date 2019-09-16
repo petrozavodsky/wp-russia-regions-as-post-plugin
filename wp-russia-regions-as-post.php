4995ae06efb02b710335955eb717bd411e8ecb2e
petrozavodsky <petrozavodsky@gmail.com>
2019-09-16 17:39:31 +0300

    Добавил константы

--- wp-russia-regions-as-post.php
+++ wp-russia-regions-as-post.php
@@ -2,3 +2,15 @@
 /*
 Plugin Name: wp russia regions as post
 */
+
+if ( ! defined( 'WP_RUSSIA_REGIONS_AS_POST_POST_TYPE' ) ) {
+    define( 'WP_RUSSIA_REGIONS_AS_POST_POST_TYPE', 'region' );
+}
+
+if ( ! defined( 'WP_RUSSIA_REGIONS_AS_POST_POST_META_KEY' ) ) {
+    define( 'WP_RUSSIA_REGIONS_AS_POST_POST_META_KEY', '_region_code' );
+}
+
+if ( ! defined( 'WP_RUSSIA_REGIONS_AS_POST_POST_TEXTDOMAIN' ) ) {
+    define( 'WP_RUSSIA_REGIONS_AS_POST_POST_TEXTDOMAIN', 'wp_russia_regions_as_post' );
+}
