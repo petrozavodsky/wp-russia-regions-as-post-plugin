284c6155b3772d9925e90b6ea80570e950112400
petrozavodsky <petrozavodsky@gmail.com>
2019-09-17 11:36:18 +0300

    push force

--- wp-russia-regions-as-post.php
+++ wp-russia-regions-as-post.php
@@ -4,6 +4,7 @@
  * Description: Show a map of Russian regions
  * Version: 1.0.0
  * Author: petrozavodsky, anatolykulikov, soulseekah
+ * Author: http
  * Text Domain: wp_russia_regions_as_post
  * License: GPL-3
  * License URI: http://www.gnu.org/licenses/gpl-3.0.txt
